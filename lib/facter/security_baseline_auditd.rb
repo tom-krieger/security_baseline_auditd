84ab49b2aeeb9a634a96393163e5a80dac74b84f
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-22 17:28:25 +0200

    changed expected outcome

--- lib/facter/security_baseline_auditd.rb
+++ lib/facter/security_baseline_auditd.rb
@@ -145,8 +145,7 @@ Facter.add('security_baseline_auditd') do
     expected = []
     Facter.value(:partitions).each do |_part, data|
       mount = data['mount']
-      rules_raw = Facter::Core::Execution.exec('find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk \'{print "-a always,exit -F path=" $1 " -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged" }\'').split("\n")
-      pp rules_raw
+      rules_raw = Facter::Core::Execution.exec('find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk \'{print "-a always,exit -S all -F path=" $1 " -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged" }\'').split("\n")
       rules[mount] = rules_raw
       expected.push(*rules_raw)
     end
