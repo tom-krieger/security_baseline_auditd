87d4bd80511f9c60546e0f09cd9cc374df9fc2b8
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-23 09:01:17 +0200

    examole yaml configuration

--- spec/classes/security_baseline_auditd_spec.rb
+++ spec/classes/security_baseline_auditd_spec.rb
@@ -3,7 +3,20 @@ require 'spec_helper'
 describe 'security_baseline_auditd' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) do
+        os_facts.merge(
+          'security_baseline_auditd' => {
+            'max_log_file' => 32,
+          }
+        )
+      end
+      let(:params) do
+        {
+          'enforce' => true,
+          'message' => 'automounting',
+          'loglevel' => 'warning',
+        }
+      end
 
       it { is_expected.to compile }
     end
