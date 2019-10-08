c08f24bcc051980e5f7e0558ec85a9fb7196bcff
Thomas Krieger <thomas.krieger@puppet.com>
2019-10-08 23:11:10 +0200

    debug output

--- lib/facter/security_baseline_auditd.rb
+++ lib/facter/security_baseline_auditd.rb
@@ -148,6 +148,7 @@ Facter.add('security_baseline_auditd') do
       if data['filesystem'] != 'swap'
         mount = data['mount']
         cmd = "find #{mount} -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk '{print \"-a always,exit -S all -F path=\" $1 \" -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged\"; }'"
+        pp cmd
         rules_raw = Facter::Core::Execution.exec(cmd).split("\n")
         priv_cmds.push(rules_raw)
         rules[mount] = rules_raw
