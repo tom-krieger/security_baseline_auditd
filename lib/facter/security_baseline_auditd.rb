dd1874d70ef33b049ce79cb88d7cb93a3ed9c7e4
Thomas Krieger <thomas.krieger@puppet.com>
2019-10-09 09:43:54 +0200

    fixed rules

--- lib/facter/security_baseline_auditd.rb
+++ lib/facter/security_baseline_auditd.rb
@@ -157,7 +157,7 @@ Facter.add('security_baseline_auditd') do
     cmd = "find /usr -xdev \\( -perm -4000 -o -perm -2000 \\) -type f | awk '{print \"-a always,exit -S all -F path=\" $1 \" -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged\"; }'"
     rules_raw = Facter::Core::Execution.exec(cmd).split("\n")
     priv_cmds.push(rules_raw)
-    rules[mount] = rules_raw
+    rules['/usr'] = rules_raw
     expected.push(*rules_raw)
     expected.uniq!
     security_baseline_auditd['priv-cmds-rules'] = rules
